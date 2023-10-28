2079bc3009de032d2e1440393faef81a33454ab1
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-10-27 21:59:41 -0400

    Change description to an array of sentences

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,7 +5,9 @@ export default function Project( {title, description, hrefSite, hrefGitHub, imag
     return (
         <section className="project">
             <h2 className="project--title">{title}</h2>
-            <p className="project--description">{description}</p>   
+            {description.map((line, index) => (
+                <p key={index} className="project--description-line">{line}</p>
+            ))}   
             <Link className="project--site-link" href={hrefSite}>Live site</Link>
             <Link className="project--github-link" href={hrefGitHub}>GitHub</Link>
             <img className="project--image" src={imageSrc} alt={imgageAlt} />
