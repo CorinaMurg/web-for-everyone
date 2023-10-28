e53dc0221195b92cf11c46d77da3316152253eb6
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-10-27 22:01:17 -0400

    Add prop types

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,6 @@
 import "./Project.css"
 import { Link } from "react-router-dom"
+import PropTypes from "prop-types"
 
 export default function Project( {title, description, hrefSite, hrefGitHub, imageAlt, imageSrc } ) {
     return (
@@ -10,7 +11,16 @@ export default function Project( {title, description, hrefSite, hrefGitHub, imag
             ))}   
             <Link className="project--site-link" href={hrefSite}>Live site</Link>
             <Link className="project--github-link" href={hrefGitHub}>GitHub</Link>
-            <img className="project--image" src={imageSrc} alt={imgageAlt} />
+            <img className="project--image" src={imageSrc} alt={imageAlt} />
         </section>
     )
 }
+
+Project.propTypes = {
+    title: PropTypes.string.isRequired,
+    description: PropTypes.arrayOf(PropTypes.string).isRequired,
+    hrefSite: PropTypes.string.isRequired,
+    hrefGitHub: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    imageAlt: PropTypes.string.isRequired,
+};
