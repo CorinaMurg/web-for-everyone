0c500377b0024c76586932b29738e8cbd5bd73b1
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-11-06 06:47:49 -0500

    Add image and intro to Accessibility page

--- src/data/a11yData.js
+++ src/data/a11yData.js
@@ -1,7 +1,7 @@
 
 export const a11yData = {
-    title: "I believe in a web where everyone has a place",
-    description: [
+    "title": "I believe in a web where everyone has a place",
+    "description": [
         "This principle drives my work in frontend development. Accessibility is not just a feature - it's a responsibility to ensure every user feels seen and valued.",
         "It’s exciting to be part of the generation that is now stepping into web development. Awareness about the need for an accessible web is more pronounced, and there’s a thriving and supportive accessibility community.",
         "In the posts that follow, I will share some of my projects and the accessibility principles I’ve adhered to, as well as my struggles in integrating specific a11y features. I am also sharing valuable resources that have helped me in learning about digital accessibility."
--- src/pages/Accessibility.jsx
+++ src/pages/Accessibility.jsx
@@ -1,11 +1,12 @@
 import { a11yData } from "../data/a11yData"
-export default function Accessibility() {
-  
+import './Accessibility.css';
+
+export default function Accessibility() {  
     return (
         <div className="accessibility">
-            <h1>I believe in a web where everyone has a place</h1>
-            <div>
-                <img></img>
+            <h1>{a11yData.title}</h1>
+            <div className="accessibility--image-div">
+                <img alt={a11yData.imageAlt} src={a11yData.imageSrc}/>
             </div>
             {a11yData.description.map((line, index) => (
                 <div className="a11y-intro" key={index}>
