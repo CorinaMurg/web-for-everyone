b18a8c530a836d07687312e7c13ef813dec4297e
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-11-15 21:05:19 -0500

    Update a11y components data

--- src/data/a11yData/a11yComponentsData.js
+++ src/data/a11yData/a11yComponentsData.js
@@ -3,42 +3,37 @@ export const a11yComponentsData = [
     {
         "title": "Button",
         "href": "https://codepen.io/Cor-Ina/pen/ZEwyyVe",
-        "hrefAriaLabel": "Link to Corina's CodePen for a code with an accessible button.",
+        "hrefAriaLabel": "Accessible button codepen.",
     },
     {
         "title": "Link",
         "href": "https://codepen.io/Cor-Ina/pen/LYqLLKJ?editors=1100",
-        "hrefAriaLabel": "Link to Corina's CodePen for a code with an accessible link.",
+        "hrefAriaLabel": "Accessible link codepen.",
     },
     {
         "title": "Accordion",
         "href": "https://codepen.io/Cor-Ina/pen/xxMrLGJ",
-        "hrefAriaLabel": "Link to Corina's CodePen for a code with an accessible accordion.",
+        "hrefAriaLabel": "Accessible accordion codepen.",
     },
     {
         "title": "Navbar",
         "href": "https://codepen.io/Cor-Ina/pen/RwvgxyP?editors=1100",
-        "hrefAriaLabel": "Link to Corina's CodePen for a code with an accessible navbar.",
+        "hrefAriaLabel": "Accessible navbar codepen.",
     },
-    // {
-    //     "title": "Form",
-    //     "href": "",
-    //     "hrefAriaLabel": ""
-    // },
     {
         "title": "Dropdown Menu",
         "href": "https://codepen.io/Cor-Ina/pen/yLZXoQj",
-        "hrefAriaLabel": "Link to Corina's CodePen for a code with an accessible dropdown menu.",
+        "hrefAriaLabel": "Accessible dropdown menu codepen.",
     },
     {
         "title": "skip link",
         "href": "https://codepen.io/Cor-Ina/pen/abXwLde",
-        "hrefAriaLabel": "Link to Corina's CodePen for a code with an accessible skip link.",
+        "hrefAriaLabel": "Accessible skip link codepen.",
     },
     {
         "title": "visually-hidden element",
-        "href": "",
-        "hrefAriaLabel": ""
+        "href": "https://codepen.io/Cor-Ina/pen/oNmoYeR",
+        "hrefAriaLabel": "Visually-hidden element codepen",
     }
     
 ];
