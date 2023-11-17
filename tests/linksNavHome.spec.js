0f33c841a7c8d56b9114ee574c0bac8c9ce081f3
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-11-16 22:47:32 -0500

    Update tests

--- tests/linksNavHome.spec.js
+++ tests/linksNavHome.spec.js
@@ -1,12 +1,25 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-    await page.goto('https://corinamurg.netlify.app/');
+    await page.goto('http://localhost:5173/');
     await page.getByRole('banner').getByRole('link', { name: 'Corina Murg' }).click();
     await page.getByRole('navigation').getByRole('link', { name: 'Home' }).click();
     await page.getByRole('navigation').getByRole('link', { name: 'Projects' }).click();
     await page.getByRole('navigation').getByRole('link', { name: 'Accessibility' }).click();
     await page.getByRole('navigation').getByRole('link', { name: 'About' }).click();
+    const page1Promise = page.waitForEvent('popup');
     await page.getByRole('navigation').getByRole('link', { name: 'Blog' }).click();
+    const page1 = await page1Promise;
+
+    
+
+    // Check if the URL has changed to the About page's URL
+    await expect(page).toHaveURL('https://corinamurg.netlify.app/about');
+
+    // Check for specific content that should appear on the About page
+    await expect(page.getByText('My Career in a Nutshell')).toBeVisible();
+
+
+    
 });
 
