87596f5cd71858d893fdf67a65fbc48a0efff432
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-11-25 15:12:31 -0500

    Update Playwright and browsers

--- tests/textInAriaLabel.spec.ts
+++ tests/textInAriaLabel.spec.ts
@@ -32,14 +32,16 @@ test('WCAG 2.5.3: Label in Name', async ({ page }) => {
     
     const elements = page.locator('a[aria-label] h1, a[aria-label] h2, a[aria-label] h3, a[aria-label] h4, a[aria-label] h5, a[aria-label] h6');
     const elementsCount = await elements.count();
-    const errors = [];
+    const errors: string[] = [];
+
 
     for (let i = 0; i < elementsCount; i++) {
         const element = elements.nth(i);
         const headingText = (await element.textContent())?.trim().split(" ").slice(0, 3).join(" ") ?? "";
         
-        // const ariaLabel = await element.evaluate(node => node.parentElement.getAttribute('aria-label')) ?? "";
-        const ariaLabel = await element.evaluate(node => node.closest('a').getAttribute('aria-label'));
+        const ariaLabel = await element.evaluate(node => node.parentElement ? node.parentElement.getAttribute('aria-label') : null);
+
+        // const ariaLabel = await element.evaluate(node => node.closest('a').getAttribute('aria-label'));
 
         console.log(`Heading text: "${headingText}", Aria-label: "${ariaLabel}"`);
 
@@ -57,115 +59,3 @@ test('WCAG 2.5.3: Label in Name', async ({ page }) => {
 });
 
 
-// test('WCAG 2.5.3: Label in Name', async ({ page }) => {
-//     await page.goto('https://yourwebsite.com/');
-    
-//     // Select all <a> elements with an aria-label attribute
-//     const links = await page.locator('[aria-label]');
-
-//     for (const link of links) {
-//         // Check for a heading child, if not found, use the text content of the <a> itself
-//         const linkText = await link.$eval('*', (el) => {
-//             const heading = el.querySelector('h1, h2, h3, h4, h5, h6');
-//             return heading ? heading.textContent?.trim() : el.textContent?.trim();
-//         }) ?? "";
-
-//         // Get the aria-label attribute
-//         const ariaLabel = await link.getAttribute('aria-label') ?? "";
-
-//         // Perform the check for Label in Name compliance
-//         expect(ariaLabel).toContain(linkText, `The aria-label "${ariaLabel}" should include the text "${linkText}".`);
-//     }
-// });
-
-
-// CHECK FOR H4 ELEMENTS
-// test('WCAG 2.5.3: Label in Name', async ({ page }) => {
-//     function normalizeString(str) {
-//         return str.toLowerCase().replace(/[\W_]+/g, " ").trim();
-//     }
-
-//     await page.goto('https://corinamurg.netlify.com/');
-//     const links = await page.locator('[aria-label]');
-
-//     for (const link of links) {
-//         const linkText = await link.$eval('*', (el) => {
-//             const heading = el.querySelector('h1, h2, h3, h4, h5, h6');
-//             return heading ? heading.textContent?.trim() : el.textContent?.trim();
-//         }) ?? "";
-
-//         const ariaLabel = await link.getAttribute('aria-label') ?? "";
-//         const normalizedAriaLabel = normalizeString(ariaLabel);
-//         const normalizedLinkText = normalizeString(linkText);
-//         const condition = normalizedAriaLabel.includes(normalizedLinkText);
-        
-//         if (!condition) {
-//             throw new Error(`The aria-label "${ariaLabel}" should include the text "${linkText}".`);
-//         }
-//         expect(condition).toBeTruthy();
-//     }
-// });
-
-// SHOW ALL ERRORS
-// test('WCAG 2.5.3: Label in Name', async ({ page }) => {
-//     function normalizeString(str) {
-//         return str.toLowerCase().replace(/[\W_]+/g, " ").trim();
-//     }
-
-//     await page.goto('https://corinamurg.netlify.com/');
-//     const links = page.locator('[aria-label]');
-//     const errors = [];
-//     const linksCount = await links.count();
-
-//     for (let i = 0; i < linksCount; i++) {
-//         const link = links.nth(i);
-
-//         const linkText = await link.evaluate((el) => {
-//             const heading = el.querySelector('h1, h2, h3, h4, h5, h6');
-//             return heading ? heading.textContent?.trim() : el.textContent?.trim();
-//         });
-        
-
-//         const ariaLabel = await link.getAttribute('aria-label') ?? "";
-//         const normalizedAriaLabel = normalizeString(ariaLabel);
-//         const normalizedLinkText = normalizeString(linkText);
-        
-//         const condition = normalizedAriaLabel.includes(normalizedLinkText);  
-//         if (!condition) {
-//             errors.push(`The aria-label "${ariaLabel}" should include the visible text "${linkText}"`);
-//         }
-//     }
-//     expect(errors).toEqual([]);
-// });
-
-
-// test('WCAG 2.5.3: Label in Name', async ({ page }) => {
-//     function normalizeString(str) {
-//         return str.toLowerCase().replace(/[\W_]+/g, " ").trim();
-//     }
-
-//     await page.goto('https://corinamurg.netlify.com/');
-//     const links = page.locator('[aria-label]');
-//     const errors = [];
-//     const linksCount = await links.count();
-
-//     for (let i = 0; i < linksCount; i++) {
-//         const link = links.nth(i);
-
-//         const linkText = await link.evaluate((el) => {
-//             const heading = el.querySelector('h1, h2, h3, h4, h5, h6');
-//             return heading ? heading.textContent?.trim() : el.textContent?.trim();
-//         });
-        
-
-//         const ariaLabel = await link.getAttribute('aria-label') ?? "";
-//         const normalizedAriaLabel = normalizeString(ariaLabel);
-//         const normalizedLinkText = normalizeString(linkText);
-        
-//         const condition = normalizedAriaLabel.includes(normalizedLinkText);  
-//         if (!condition) {
-//             errors.push(`The aria-label "${ariaLabel}" should include the visible text "${linkText}"`);
-//         }
-//     }
-//     expect(errors).toEqual([]);
-// });
