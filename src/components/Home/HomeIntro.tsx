6321fef27a4ccb1e1eea3dabd2d6a660e713b97a
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-11-26 00:09:36 -0500

    Update heading font

--- src/components/Home/HomeIntro.tsx
+++ src/components/Home/HomeIntro.tsx
@@ -4,7 +4,7 @@ import './HomeIntro.css';
 export default function HomeIntro() {
     return (
         <div className="home-intro">
-            <h1 className="page-heading update-padding">Hello, I'm Corina!</h1>
+            <h1 className="page-heading update-padding"><span>Hello, </span><span>I'm Corina!</span></h1>
             
             <div className="subheading-div update-margin">
                 <p className='subheading-p'>A former high-school math teacher and self-taught developer focused on creating accessible web products. 
