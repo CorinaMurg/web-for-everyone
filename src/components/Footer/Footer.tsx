65316d3ffc90110bf33c19c45d5a9302e6491a78
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2024-02-02 11:55:34 -0500

    Update links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import scrollToTop from "../../utils/scrollToTop"
+import WELogo from "../WELogo/WELogo"
 import "./Footer.css"
 
 export default function Footer() {
@@ -9,43 +10,28 @@ export default function Footer() {
             <div className="footer-top">
                 <h2 className="sr-only">Footer</h2>
                 <div className="footer-left">
-                    <Link to="/" onClick={scrollToTop} className="logo">WE <span className="logo-vertical">|</span>Web for Everyone</Link>
-                    
+                    {/* <Link to="/" onClick={scrollToTop} className="logo">WE <span className="logo-vertical">|</span>Web for Everyone</Link> */}
+                    <Link className="logo" to="/">
+                        <WELogo size="1.7rem"/>
+                        <span className="logo-vertical"> | </span> 
+                        Web for Everyone
+                    </Link>
                 </div>
                 <div className="footer-right">
                     <div className="footer-right--navigation">
-                        <span className="footer-heading">Site</span>
+                        <h3 className="footer-heading">Site Links</h3>
                         <ul>
                             <li><Link to="/" onClick={scrollToTop}><span className="footer-nav-link">Home</span></Link></li>
-                            <li><Link to="/projects" onClick={scrollToTop}><span className="footer-nav-link">Projects</span></Link></li>
-                            <li><Link to="/accessibility" onClick={scrollToTop}><span className="footer-nav-link">Accessibility</span></Link></li>
+                            <li><Link to="/projects" onClick={scrollToTop}><span className="footer-nav-link">Your Users</span></Link></li>
+                            <li><Link to="/accessibility" onClick={scrollToTop}><span className="footer-nav-link">Resources</span></Link></li>
                             <li><Link to="/about" onClick={scrollToTop}><span className="footer-nav-link">About</span></Link></li>
                         </ul>
                     </div>
                     <div className="footer-right--social">
-                        <span className="footer-heading">Profiles</span>
-                        <ul>
-                            <li>
-                                <a href="https://www.linkedin.com/in/corinamurg/" target="_blank" rel="noopener noreferrer">
-                                    <span className="footer-nav-link">LinkedIn</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://github.com/CorinaMurg" target="_blank" rel="noopener noreferrer">
-                                    <span className="footer-nav-link">GitHub</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://twitter.com/CorinaMurg" target="_blank" rel="noopener noreferrer">
-                                    <span className="footer-nav-link">Twitter</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://dev.to/corinamurg" target="_blank" rel="noopener noreferrer">
-                                    <span className="footer-nav-link">Blog</span>
-                                </a>
-                            </li>
-                        </ul>
+                        <h3 className="footer-heading">Contact</h3>
+                        <a href="https://www.linkedin.com/in/webforeveryone/" target="_blank" rel="noopener noreferrer">
+                            <span className="footer-nav-link">LinkedIn</span>
+                        </a>
                     </div>
                 </div>
             </div>
