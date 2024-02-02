52db79cfd412851528af593b8255f8e1ad05cd42
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2024-02-02 08:23:20 -0500

    Update text content

--- src/data/resourcesData/resourcesPostsData.ts
+++ src/data/resourcesData/resourcesPostsData.ts
@@ -28,7 +28,7 @@ export const resourcesPostsData: ResourcesPostDataTypes[] = [
     {
         "label": "understanding-accessibility",
         "id": "how-accessibility-works",
-        "title": "How Does Accessibility Work?",
+        "title": "How Accessibility Works",
         "subtitle": "",
         "author": "By Web for Everyone",
         "description": `Have you met the DOM' powerful sibling, the accessibility tree? 
@@ -70,10 +70,9 @@ export const resourcesPostsData: ResourcesPostDataTypes[] = [
         "id": "most-common-bugs",
         "title": "The Most Common Accessibility Bugs",
         "subtitle": "Findings from the 2023 WebAIM Million Study",
-        "author": "Web for Everyone",
-        "description": `In most conversations about accessibility, the use of semantic HTML is most 
-            likely to be the first and most common piece of advice. My post looks at the role it 
-            plays in accessibility and beyond.`,
+        "author": "By Web for Everyone",
+        "description": `The bad news: for five years in a row, a handful of bugs has been responsible for the 
+            majority of accessibility issues. The good news: they are quite easy to avoid or fix!`,
         "to": `/resources/most-common-bugs`,
         "ariaLabel": "The Most Common Accessibility Bugs article by Web for Everyone.",
         "readMoreAriaLabel": "",
@@ -95,7 +94,7 @@ export const resourcesPostsData: ResourcesPostDataTypes[] = [
         "label": "html-for-accessibility",
         "title": "The Importance of Semantic HTML",
         "subtitle": "",
-        "author": "By Corina Murg",
+        "author": "By Corina Murg on dev.to",
         "description": `In most conversations about accessibility, the use of semantic HTML is most 
             likely to be the first and most common piece of advice. My post looks at the role it 
             plays in accessibility and beyond.`,
@@ -107,7 +106,7 @@ export const resourcesPostsData: ResourcesPostDataTypes[] = [
         "label": "html-for-accessibility",
         "title": "HTML: Buttons or Links? How to Choose",
         "subtitle": "",
-        "author": "By Corina Murg",
+        "author": "By Corina Murg on dev.to",
         "description": `Buttons and links are fundamental elements in web design, each with distinct semantic 
             meanings and accessibility implications. For accessibility, it's essential to use the appropriate 
             element given the functionality you aim to implement. Let’s see how to use them!`,
@@ -121,8 +120,7 @@ export const resourcesPostsData: ResourcesPostDataTypes[] = [
         "title": "How to Design Great Alt Text: An Introduction",
         "subtitle": "",
         "author": "By Caitlin Geier on deque.com/blog",
-        "description": `This article from Deque on writing effective alt text emphasizes its importance in web 
-            accessibility. Alt text is essential users relying on screen readers or slow internet connections. 
+        "description": `Alt text is essential for users relying on screen readers or slow internet connections. 
             Learn to distinguish between decorative and informative images, 
             and how the context of an image determines the need for alt text, which is also a ranking factor for SEO.`,
         "href": `https://www.deque.com/blog/great-alt-text-introduction/`,
@@ -133,7 +131,7 @@ export const resourcesPostsData: ResourcesPostDataTypes[] = [
         "label": "html-for-accessibility",
         "title": "HTML: Tag vs. Element",
         "subtitle": "",
-        "author": "By Corina Murg",
+        "author": "By Corina Murg on dev.to",
         "description": `It’s common for web developers to use HTML element and HTML tag interchangeably. 
             But are they really the same thing? Or is there more than meets the eye? Let's decode this lingo!`,
         "to": `/resources/tag-vs-element`,
@@ -142,10 +140,10 @@ export const resourcesPostsData: ResourcesPostDataTypes[] = [
     },
     {
         "label": "css-for-accessibility",
-        "title": "Is \"rem\" Always the Best Choice for Accessibility?",
+        "title": "Is \"rem\" Always the Best Choice?",
         "subtitle": "",
         "author": "By Ashlee M Boyer on ashleemboyer.com",
-        "description": `While rem units are often championed for their scalability, particularly for 
+        "description": `While rem units are great for 
             text to aid with zoom and accessibility, they may not be the best choice for margins, padding, 
             and spacing. Read Ashlee M Boyer's post on how using px units in these cases can 
             lead to a more predictable and user-friendly layout.`,
@@ -170,8 +168,8 @@ export const resourcesPostsData: ResourcesPostDataTypes[] = [
     {
         "label": "accessible-digital-presence",
         "title": "Your Portfolio Site: Let's Make It Accessible",
-        "subtitle": "Part One: Finding the 7 Most Common Bugs",
-        "author": "Corina Murg",
+        "subtitle": "Part One: Finding the 6 Most Common Bugs",
+        "author": "By Web for Everyone",
         "description": `You're adding the finishing touches to your portfolio site. But will it be ready 
                 to welcome all visitors? Let's check for the most common accessibility bugs.`,
         "to": `/resources/make-it-accessible-part1`,
@@ -181,9 +179,9 @@ export const resourcesPostsData: ResourcesPostDataTypes[] = [
     {
         "label": "accessible-digital-presence",
         "title": "Your Portfolio Site: Let's Make It Accessible",
-        "subtitle": "Part 2: On the hunt for 7 more bugs",
-        "author": "Corina Murg",
-        "description": `You've fixed the 7 most common accessibility bugs. Can we find 7 more? Will they be
+        "subtitle": "Part 2: On the hunt for 6 more bugs",
+        "author": "By Web for Everyone",
+        "description": `You've fixed the 6 most common accessibility bugs. Can we find 6 more? Will they be
             as easy to fix? Let's learn where to look and how to avoid them.`,
         "to": `/resources/make-it-accessible-part2`,
         "ariaLabel": "",
@@ -194,8 +192,8 @@ export const resourcesPostsData: ResourcesPostDataTypes[] = [
         "title": "Tips for an Accessible GitHub Profile",
         "subtitle": "",
         "author": "By Annie Streater on github.blog",
-        "description": `This GitHub blog post provides practical advice on how to ensure your GitHub profile 
-            is navigable and understandable by all users, including those with disabilities. You'll find tips '
+        "description": `A post with practical advice on how to ensure your GitHub profile 
+            is navigable and understandable by all users, including those with disabilities. You'll find tips
             on link context, image alt text, heading structure, plain language, and use of emojis. Enjoy!`,
         "href": "https://github.blog/2023-10-26-5-tips-for-making-your-github-profile-page-accessible/",
         "ariaLabel": "5 Tips for Making Your GitHub Profile Page Accessible, GitHub blog post.",
