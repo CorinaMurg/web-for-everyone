02aa6bb4d78dbd13ebf7fb755ac65e7d09ef0d32
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2024-02-15 22:26:47 -0500

    Update breadcrums styles

--- src/components/BackLinks/BackLinks.tsx
+++ src/components/BackLinks/BackLinks.tsx
@@ -17,7 +17,7 @@ export default function BackLinks ({secondLink, href, current}: {secondLink: str
                     </Link>
                 </li>
                 <li>
-                    <a href="" aria-current="page">
+                    <a aria-current="page" tabIndex={0}>
                         <code>{current}</code>
                     </a>
                 </li>
@@ -25,18 +25,3 @@ export default function BackLinks ({secondLink, href, current}: {secondLink: str
         </nav>
     );
 };
-
-{/* <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="15"
-                height="15"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            >
-                <polyline points="9 18 15 12 9 6"></polyline>
-            </svg>
-             */}
