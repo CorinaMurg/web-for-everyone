9bfaf1abad9f79ce007a21a2980459bcfb01fcc6
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2024-02-17 22:45:16 -0500

    Remove scroll from TOC, remove scroll-target from backlinks

--- src/components/BackLinks/BackLinks.tsx
+++ src/components/BackLinks/BackLinks.tsx
@@ -4,7 +4,7 @@ import styles from './BackLinks.module.css';
 
 export default function BackLinks ({secondLink, href, current}: {secondLink: string; href: string; current: string;}) {
     return (
-        <nav aria-label="Breadcrumb" className={`${styles['back-links']} scroll-target`} id="back-links">
+        <nav aria-label="Breadcrumb" className={`${styles['back-links']}`} id="back-links">
             <ol>
                 <li>
                     <Link to="/resources" className={`${styles['back-link']} hover-pink`}>
