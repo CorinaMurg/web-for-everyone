d6a2b7ec82f78dfaa3a9656273f509246409d174
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2024-03-04 07:15:23 -0500

    Update not-found styles

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,12 +2,14 @@
 import React from "react"
 import useDocTitle from "../hooks/useDocTitle"
 import { Link } from "react-router-dom"
+import styles from './Home.module.css'
+import "../global.css"
 
 export default function NotFound() {
     useDocTitle("Not Found | Web for Everyone");
 
     return (
-        <div className="not-found-container">
+        <div className={styles['not-found-container']}>
             <h1>Sorry, the page you were looking for was not found.</h1>
             <Link to="/" className="link-button">Return to Home</Link>
         </div>
