61c98749a2c02730d8bdc6760a579e731df54a35
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2024-03-24 17:14:33 -0400

    Add onClick to Logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useScrollToTop from '../../hooks/useScrollToTop';
 import styles from './Logo.module.css';
 
 interface LogoTypes {
     color: string,
     fontWeight: number,
+    closeModal?: () => void,
 }
 
-export default function Logo( { color, fontWeight }: LogoTypes ) {
+export default function Logo( { color, fontWeight, closeModal }: LogoTypes ) {
 
     const logoColor = {
         color: color,
@@ -15,7 +17,11 @@ export default function Logo( { color, fontWeight }: LogoTypes ) {
     }
 
     return (
-        <Link className={styles['logo-link']} to="/">
+        <Link className={styles['logo-link']} 
+            to="/"
+            onClick={() => { useScrollToTop(); if (closeModal) {closeModal();}}}
+        
+        >
             <div className={styles['logo-container']}>
                 <img src="/assets/faviconDull.png" alt="" />
             </div>
