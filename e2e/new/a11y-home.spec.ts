d782d9a391f25dc1b9a92150e6d60652df83cfb0
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2024-04-12 12:33:31 -0400

    Update tests

---
+++ e2e/new/a11y-home.spec.ts
@@ -0,0 +1,42 @@
+
+import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright'; 
+import { Page } from 'playwright-core';
+
+// https://dequeuniversity.com/rules/axe/html/4.8
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://webforeveryone.us/');
+});
+
+test.describe('a11y home', () => { 
+    test('no wcagAA issues ', async ({ page }) => {   
+        const accessibilityScanResults = await new AxeBuilder({ page: page as Page })
+            .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+            .analyze(); 
+
+        expect(accessibilityScanResults.violations).toEqual([]); 
+    });
+    test('no wcagAAA issues ', async ({ page }) => {   
+        const accessibilityScanResults = await new AxeBuilder({ page: page as Page })
+            .withTags(['wcag2aaa'])
+            .disableRules(['color-contrast-enhanced'])
+            .analyze(); 
+
+        expect(accessibilityScanResults.violations).toEqual([]); 
+    });
+    test('no best practice issues ', async ({ page }) => {   
+        const accessibilityScanResults = await new AxeBuilder({ page: page as Page })
+            .withTags(['best-practice'])
+            .analyze(); 
+
+        expect(accessibilityScanResults.violations).toEqual([]); 
+    });
+    test('no experimental issues ', async ({ page }) => {   
+        const accessibilityScanResults = await new AxeBuilder({ page: page as Page })
+            .withTags(['experimental'])
+            .analyze(); 
+
+        expect(accessibilityScanResults.violations).toEqual([]); 
+    });
+});
---
+++ e2e/new/keyboard.spec.ts
@@ -0,0 +1,8 @@
+
+
+// await page.getByRole("link", { name: "Home", exact: true }).focus();
+
+// await expect(page.getByRole("link", { name: "Home", exact: true })).toBeFocused();
+
+// await page.keyboard.press("Tab");
+// await expect(page.getByRole("link", { name: "News", exact: true })).toBeFocused();
