5bbee12ef7ffdd2c5580c5d2989ec0336d0162fa
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2024-06-04 08:18:49 -0400

    Update tests

--- src/components/MenuModal/menuModal.spec.ts
+++ src/components/MenuModal/menuModal.spec.ts
@@ -20,5 +20,32 @@ test.describe("Modal menu", () => {
     }
   });
 
+  test("modal menu controls", async ({ page }) => {
+
+    if(isMobileViewport({page})) {
+        const menu = new MenuModalPageObject(page);
+        await menu.goToMenuButton();
+        
+
+        await locateClickStore(page, "header", "link", "Home");
+        await expect(page).toHaveTitle(/Web for Everyone/);
+        await expect(page.getByRole("heading", { name: "Let's make it accessible." })).toBeVisible();
+
+        await locateClickStore(page, "header", "link", "Blog");
+        await expect(page).toHaveTitle(/Blog/);
+        await expect(page.getByRole("heading", { name: "Accessibility resources" })).toBeVisible();
+
+        await locateClickStore(page, "header", "link", "About");
+        await expect(page).toHaveTitle(/About/);
+        await expect(page.getByRole("heading", { name: "Let's make it accessible." })).toBeVisible();
+    }
+
+    
+  });
+
+  test("modal menu controls should be focusable", async ({ page }) => {
+    await checkFocus(page);
+  });
+
   
 });
