8193776d3a10aa92912dbb4061528861e229ddc7
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2024-09-16 16:52:26 -0400

    Add/remove comments

--- src/hooks/useTabindexOnMobile.tsx
+++ src/hooks/useTabindexOnMobile.tsx
@@ -5,15 +5,10 @@ export default function useTabindexOnMobile(maxWidth = 600) {
     const [isMobile, setIsMobile] = useState(window.innerWidth <= maxWidth);
 
     useEffect(() => {
-        // set state based on window size
         function readWindowSize() {
             setIsMobile(window.innerWidth <= maxWidth);
         }
 
-        // While React efficiently updates the DOM based on state changes, it does not inherently know about 
-        // external events unless you set up listeners for them. 
-        // By adding window.addEventListener('resize', handleResize), you instruct the browser to call 
-        // the handleResize function every time the window size changes.
         window.addEventListener('resize', readWindowSize);
 
         return () => {
