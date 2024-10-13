1673ed30dcf47df6d5b5794aa04557b6a931fc92
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2024-10-12 21:23:35 -0400

    Add text to scrollToTopButton

--- src/components/ScrollToTopButton/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton/ScrollToTopButton.tsx
@@ -29,7 +29,7 @@ export default function ScrollToTopButton() {
     return (
         isVisible && <div>
             <button onClick={scrollToTop} className={styles['scroll-to-top-button']}>
-                <span className="sr-only">Scroll to top</span>
+                <span className="back-to-top">To top</span>
                 <svg width="15px" height="20" viewBox="0 0 384 512">
                     <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2 160 448c0 17.7 14.3 32 32 32s32-14.3 32-32l0-306.7L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/>
                 </svg>
